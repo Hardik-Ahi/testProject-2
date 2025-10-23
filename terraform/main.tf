6425a3ea113ff665bf902c65e84997fa51654b7a
Hardik-Ahi <ahi.hardik@gmail.com>
2025-10-23 13:59:31 +0530

    add port range in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,4 +43,5 @@ resource "aws_vpc_security_group_ingress_rule" "ingress" {
   security_group_id = aws_security_group.allow_traffic.id
   ip_protocol = "tcp"
   cidr_ipv4 = "0.0.0.0/0"
+  from_port = 0  # start of port range
 }
