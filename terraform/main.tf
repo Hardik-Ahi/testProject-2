7461ca75172eae81fd22f6edae456d960b4a4938
Hardik-Ahi <ahi.hardik@gmail.com>
2025-10-24 15:17:53 +0530

    add ingress and *egress* rule for ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,18 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami = "ami-0f9708d1cd2cfee41"
   instance_type = "t3.micro"
-  key_name = "public-key-ec2"
+  key_name = aws_key_pair.keyPair.key_name
   vpc_security_group_ids = [ aws_security_group.allow_traffic.id ]
 
   user_data = <<-EOF
   #!/bin/bash
-  sudo dnf update -y
-  sudo dnf install -y docker
-  sudo systemctl start docker
+  sudo yum update -y
+  sudo yum install -y docker
+  sudo service docker start
   sudo systemctl enable docker
   sudo usermod -aG docker ec2-user
   sudo -u ec2-user docker pull ahihardik11/html:latest
-  sudo -u ec2-user docker run -p 9090:80 ahihardik11/html:latest
+  sudo -u ec2-user docker run -d -p 9090:80 ahihardik11/html:latest
   EOF
 }
 
@@ -39,10 +39,22 @@ resource "aws_key_pair" "keyPair" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBkEfAiTo4isYALYxDm5Uv04VZ3TLwfI1luOTMugIee ahiha@AcerNitro5"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress" {
+resource "aws_security_group_rule" "ingress" {
+  type = "ingress"
+  protocol = -1
   security_group_id = aws_security_group.allow_traffic.id
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
   from_port = 0  # start of port range
   to_port = 65535  # end of port range
+  cidr_blocks = [ "0.0.0.0/0" ]
+  ipv6_cidr_blocks = [ "::/0" ]
+}
+
+resource "aws_security_group_rule" "egress" {
+  type = "egress"
+  protocol = -1
+  security_group_id = aws_security_group.allow_traffic.id
+  from_port = 0  # start of port range
+  to_port = 65535  # end of port range
+  cidr_blocks = [ "0.0.0.0/0" ]
+  ipv6_cidr_blocks = [ "::/0" ]
 }
